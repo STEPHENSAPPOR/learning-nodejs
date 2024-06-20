fa5707f65e745ebb645047a66c238a5a1c15cb57
STEPHENSAPPOR <STEPHENSAPPOR5@GMAIL.COM>
2024-06-20 13:22:12 +0000

    File system in nodejs

---
+++ deleteFile.mjs
@@ -0,0 +1,6 @@
+import {unlink} from 'node:fs';
+
+unlink('./hello.html', () => {
+    console.log('File deleted');
+});
+
--- server.mjs
+++ server.mjs
@@ -1,13 +1,31 @@
 import {createServer} from 'node:http';
+import {unlink} from 'node:fs';
 
 // Define request handler
 
 
 // Create http server
-const server = createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html'});
-    res.end('<h1>We have an HTTP Server</h1>');
-});
+const server = createServer((req, res) => {
+    console.log(req.url);
+    if (req.url.includes('create')) {
+        // create file
+        writeFile('./abc.html', '<h1>Learning Node.js</h1>', (err) => {
+            console.log(err);
+        // return response
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        res.end('<h1>We have an HTTP Server</h1>');
+   });
+} else {
+    // Delete file
+    unlink('./index.html', () => {
+        // return response
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        res.end('<h1>We have an HTTP Server</h1>');
+ } 
+)}
+}
+);
+    
 
 // Listen for incoming request
 server.listen(3000, '127.0.0.1', () => {
